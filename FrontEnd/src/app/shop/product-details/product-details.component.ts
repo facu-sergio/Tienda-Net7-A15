e890bb6a68609b37a1953ebbe755e732a137e329
facu <facu11ja@gmail.com>
2023-05-27 17:38:39 -0300

    Se agrega al frontend varias mejoras, breadcrumb, encabezados, tema de bootstrap, indicador de carga y pagina de inicio

--- FrontEnd/src/app/shop/product-details/product-details.component.ts
+++ FrontEnd/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { Producto } from 'src/app/shared/models/producto';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -9,15 +10,24 @@ import { Producto } from 'src/app/shared/models/producto';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute){}
   producto?: Producto;
+
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute,
+     private bcService: BreadcrumbService){
+      this.bcService.set('@productDetails', ' ');
+     }
+
   ngOnInit(): void {
     this.loadProuct();
   }
   loadProuct(){
     const id =  this.activateRoute.snapshot.paramMap.get('id');
     if(id) this.shopService.getProducto(+id).subscribe({
-      next: producto => this.producto = producto,
+      next: producto => {
+        this.producto = producto;
+        this.bcService.set('@productDetails', producto.nombre)
+
+      },
       error: err => console.log(err)
     });
   }
